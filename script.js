4f9aafc2baf41ca2f2306b5a03ba8b683a49da6e
Abhishek Bhatt <abhishekbhatt625@gmail.com>
2022-06-30 11:20:29 +0545

    javaScript tutorial

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-console.log('hello world');
-//alert("Abhishek");
-// var text = (document.getElementsByTagName('p')[1].innerHTML = 'new text');
-// text.innerHTML = 'new text';
 
-document.write('hello');
-
-console.warn('this is a warning');
-console.error('this is a error');
-document.write('this is document');
-
-var number1 = 20;
-var number2 = 30;
-console.log('number1=number2');
